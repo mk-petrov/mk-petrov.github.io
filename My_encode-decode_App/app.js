77f04bef83e55a2431ae6bb99250d4503de4075d
mk-petrov <petko.petrov89@gmail.com>
2018-05-23 17:35:36 +0200

    initial commit simple js app

---
+++ My_encode-decode_App/app.js
@@ -0,0 +1,20 @@
+function encode(){    
+    let toEncode = document.getElementById('encode').value;    
+    let resultFromEncode = document.getElementById('textArea');
+    
+    for(let i =0; i < 15; i++){
+        toEncode = btoa(toEncode);        
+    }
+    resultFromEncode.textContent = toEncode;    
+}
+
+function decode(){
+    let toDecode = document.getElementById('textAreaDecode').value;
+    let resultFromDecode = document.getElementById('decodingResult');
+    for(let i = 15; i > 0; i--){
+        toDecode = atob(toDecode);
+        
+    }
+
+    resultFromDecode.textContent = toDecode;
+}
