146f91639ef57335af8a92b069f16e7f407e7fa8
mk-petrov <petko.petrov89@gmail.com>
2018-05-24 12:15:12 +0200

    some style added and bug fixed

--- My_encode-decode_App/app.js
+++ My_encode-decode_App/app.js
@@ -1,20 +1,30 @@
-function encode(){    
-    let toEncode = document.getElementById('encode').value;    
-    let resultFromEncode = document.getElementById('textArea');
+function solve(){
+    let btnEncode = document.getElementById('btnEncode');
+    let btnDecode = document.getElementById('btnDecode');    
+    btnEncode.addEventListener('click', encode);
+    btnDecode.addEventListener('click', decode);
     
-    for(let i =0; i < 15; i++){
-        toEncode = btoa(toEncode);        
-    }
-    resultFromEncode.textContent = toEncode;    
-}
 
-function decode(){
-    let toDecode = document.getElementById('textAreaDecode').value;
-    let resultFromDecode = document.getElementById('decodingResult');
-    for(let i = 15; i > 0; i--){
-        toDecode = atob(toDecode);
+    function encode(){    
+        let toEncode = document.getElementById('encode').value;    
+        let resultFromEncode = document.getElementById('textArea');
+        
+        for(let i =0; i < 15; i++){
+            toEncode = btoa(toEncode);        
+        }
+        resultFromEncode.textContent = toEncode;    
+    }
+    
+    function decode(){
+        let toDecode = document.getElementById('textAreaDecode').value;
+        let resultFromDecode = document.getElementById('decodingResult');
+        for(let i = 15; i > 0; i--){
+            toDecode = atob(toDecode);
+            
+        }
+    
+        resultFromDecode.textContent = toDecode;
+        resultFromDecode.style.display = 'inline';
         
     }
-
-    resultFromDecode.textContent = toDecode;
 }
