1d02965533066bc0b282d617d8b645e993987436
mk-petrov <petko.petrov89@gmail.com>
2019-01-19 11:41:55 +0200

    initial commit

---
+++ FlickrApi/precache-manifest.63f5af6d5e2f897f1a4190131716fd3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2faf1ca9846c62ebbfa3",
+    "url": "/FlickrApi/static/js/runtime~main.2faf1ca9.js"
+  },
+  {
+    "revision": "684b1f87e8a1b9e77683",
+    "url": "/FlickrApi/static/js/main.684b1f87.chunk.js"
+  },
+  {
+    "revision": "9be04e9d242b63b6f37e",
+    "url": "/FlickrApi/static/js/1.9be04e9d.chunk.js"
+  },
+  {
+    "revision": "684b1f87e8a1b9e77683",
+    "url": "/FlickrApi/static/css/main.57e0b298.chunk.css"
+  },
+  {
+    "revision": "9be04e9d242b63b6f37e",
+    "url": "/FlickrApi/static/css/1.68a14b5e.chunk.css"
+  },
+  {
+    "revision": "63853e668ea9bce3dc672439be60dbd8",
+    "url": "/FlickrApi/index.html"
+  }
+];
